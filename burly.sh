aa1047abe1dcaeced2214b8d095ec4205a31c94b
Justin Ross <jross@apache.org>
2022-10-30 08:44:31 -0400

    WIP

--- burly.sh
+++ burly.sh
@@ -243,7 +243,7 @@ generate_password() {
     assert program_is_available head
     assert program_is_available tr
 
-    head -c 1024 /dev/urandom | LC_ALL=C tr -dc 'a-z0-9' | head -c 16
+    head -c 1024 /dev/urandom | LC_ALL=C tr -dc "a-z0-9" | head -c 16
 }
 
 enable_strict_mode() {
